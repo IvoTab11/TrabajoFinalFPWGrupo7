e91da98f1a9cb9d2bfe10db8270f49e101ebbb78
IvoTab11 <142843560+IvoTab11@users.noreply.github.com>
2023-11-19 11:41:03 -0300

    Se agregaron los comentarios en el proyecto 'Notas' y se aplicó el identado

--- src/components/reactComponents/Notas.js
+++ src/components/reactComponents/Notas.js
@@ -2,23 +2,25 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function Notas({notas, eliminarNota, procesarNota}){
+//Muestra las notas guardadas en el array "notas".
+function Notas({ notas, eliminarNota, procesarNota }) {
 
-    return (
-        <ul style={{justifyContent: 'center', alignItems: 'center'}}>
-                {notas.map((nota, index) => (
-                    <Card key={index} style={{ width: '18rem' }}>
-                    <Card.Body>
-                      <Card.Title>{nota.titulo}</Card.Title>
-                      <Card.Text>
-                            {nota.descripcion}<br></br>
-                      </Card.Text>
-                      <Button onClick={() => eliminarNota(index)} variant="warning">Eliminar Nota</Button>
-                      <Button onClick={()=> procesarNota(index)}>En Proceso</Button>
-                    </Card.Body>
-                  </Card>
-                ))}
-            </ul>
-    );
+  return (
+    <ul style={{ justifyContent: 'center', alignItems: 'center' }}>
+      {/* Recorre el array "notas" y muestra cada elemento en un Card. */}
+      {notas.map((nota, index) => (
+        <Card key={index} style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>{nota.titulo}</Card.Title>
+            <Card.Text>
+              {nota.descripcion}<br></br>
+            </Card.Text>
+            <Button onClick={() => eliminarNota(index)} variant="warning">Eliminar Nota</Button>
+            <Button onClick={() => procesarNota(index)}>En Proceso</Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </ul>
+  );
 }
 export default Notas;
--- src/components/reactComponents/NotasResueltas.js
+++ src/components/reactComponents/NotasResueltas.js
@@ -2,21 +2,23 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function NotasResueltas({notasResueltas,eliminarNotaResuelta}){
-    return (
-        <ul style={{justifyContent: 'center', alignItems: 'center'}}>
-                {notasResueltas.map((nota, index) => (
-                    <Card key={index} style={{ width: '18rem' }}>
-                    <Card.Body>
-                      <Card.Title>{nota.titulo}</Card.Title>
-                      <Card.Text>
-                            {nota.descripcion}<br></br>
-                      </Card.Text>
-                      <Button onClick={() => eliminarNotaResuelta(index)} variant="warning">Eliminar Nota</Button>
-                    </Card.Body>
-                  </Card>
-                ))}
-            </ul>
-    );
+//Muestra las notas guardadas en el array "notasResueltas".
+function NotasResueltas({ notasResueltas, eliminarNotaResuelta }) {
+  return (
+    <ul style={{ justifyContent: 'center', alignItems: 'center' }}>
+      {/* Recorre el array "notasResueltas" y muestra cada elemento en un Card. */}
+      {notasResueltas.map((nota, index) => (
+        <Card key={index} style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>{nota.titulo}</Card.Title>
+            <Card.Text>
+              {nota.descripcion}<br></br>
+            </Card.Text>
+            <Button onClick={() => eliminarNotaResuelta(index)} variant="warning">Eliminar Nota</Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </ul>
+  );
 }
 export default NotasResueltas;
