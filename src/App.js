61c31f714b8ec7ee77dbfbed9e2a7e7b9ef27eb3
celidearaceli <celidearaceli@gmail.com>
2023-11-19 22:25:11 -0300

    Cambios en la navegador

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router,Routes,Route,Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Inicio from "./components/Inicio";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -10,42 +10,42 @@ import AppReactAnimals from "./components/AppReactAnimals";
 import ErrorPage from "./components/ErrorPage";
 
 
-function App(){
 
-return(
+function App() {
+
+  return (
     <Router>
-        <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar className="bg-body-tertiary">
         <Container>
-        <Navbar.Brand href="/">Grupo 777</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/ProyectoPhaser">Dude</Nav.Link>
-            <Nav.Link href="/ProyectoNotas">Anotador</Nav.Link>
-            <Nav.Link href="#home">Naves</Nav.Link>
-            <Nav.Link href="/WhatAnimalIs">What Animal is?</Nav.Link>
-            <Nav.Link href="/ComparadorPrecios">Comparador De Precios</Nav.Link>
-          
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-        <Routes>
-          <Route path="/" element = {< Inicio/>} />
-          <Route path="WhatAnimalIs" element = {< AppReactAnimals/>} />
-          <Route path="ProyectoPhaser" element = {< AppPhaser/>} />
-          <Route path="ProyectoNotas" element = {< AppReact/>} />
-          <Route path="ComparadorPrecios" element = {< AppReactComp/>} />
-          <Route path="*" element = {< ErrorPage/>} />
+          <Navbar.Brand href="/">Inicio</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <Nav.Link href="/ProyectoPhaser">Dude</Nav.Link>
+              <Nav.Link href="/ProyectoNotas">Anotador</Nav.Link>
+              <Nav.Link href="#home">Naves</Nav.Link>
+              <Nav.Link href="/WhatAnimalIs">What Animal is?</Nav.Link>
+              <Nav.Link href="/ComparadorPrecios">Comparador De Precios</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={< Inicio />} />
+        <Route path="WhatAnimalIs" element={< AppReactAnimals />} />
+        <Route path="ProyectoPhaser" element={< AppPhaser />} />
+        <Route path="ProyectoNotas" element={< AppReact />} />
+        <Route path="ComparadorPrecios" element={< AppReactComp />} />
+        <Route path="*" element={< ErrorPage />} />
 
 
-        </Routes>
+      </Routes>
     </Router>
 
 
 
 
-);
+  );
 
 
 
